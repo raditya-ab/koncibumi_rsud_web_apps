5359b68138acb488d27e5b673baf08ea47af5d0e
joseptambunan <josep.tambunan7@gmail.com>
2020-10-25 19:48:53 +0700

    add script

--- application/modules/api/controllers/Profile.php
+++ application/modules/api/controllers/Profile.php
@@ -18,66 +18,74 @@ class Profile extends CI_Controller {
 	    $this->load->model('master_model','master');
 	    $this->config->load('config');
 
-	    $auth = $this->access->check_header();
-	    if ( $auth != true ){
-	    	header("HTTP/1.1 401");
-	    	$data['code'] = "401";
-	    	$data['message'] = "HEADER NOT ALLOWED";
-	    	echo json_encode($data);
-	    	exit;
-	    }
+	    if ( $_SERVER['REQUEST_METHOD'] != "OPTIONS"){
+	    	$auth = $this->access->check_header();
+		    if ( $auth != true ){
+		    	header("HTTP/1.1 401");
+		    	$data['code'] = "401";
+		    	$data['message'] = "HEADER NOT ALLOWED";
+		    	echo json_encode($data);
+		    	exit;
+		    }
 
-	    if ( !(isset($_SERVER['HTTP_TOKEN']))) {
-	    	header("HTTP/1.1 401");
-	    	$data['code'] = "401";
-	    	$data['message'] = "HEADER TOKEN NOT AVAILABLE";
-	    	echo json_encode($data);
-	    	exit;
+		    if ( !(isset($_SERVER['HTTP_TOKEN']))) {
+		    	header("HTTP/1.1 401");
+		    	$data['code'] = "401";
+		    	$data['message'] = "HEADER TOKEN NOT AVAILABLE";
+		    	echo json_encode($data);
+		    	exit;
+		    }
 	    }
+	    
 	}
 
 	public function detail_profile(){
 
-		$access_token = $_SERVER['HTTP_TOKEN'];
-		if ( $this->profile->check_token($access_token) == false ){
-			header("HTTP/1.1 401");
-	    	$data['code'] = "401";
-	    	$data['message'] = "HEADER TOKEN NOT MATCH";
-	    	echo json_encode($data);
-	    	exit;
-		}
+		if ( $_SERVER['REQUEST_METHOD'] != "OPTIONS"){
+			$access_token = $_SERVER['HTTP_TOKEN'];
+			if ( $this->profile->check_token($access_token) == false ){
+				header("HTTP/1.1 401");
+		    	$data['code'] = "401";
+		    	$data['message'] = "HEADER TOKEN NOT MATCH";
+		    	echo json_encode($data);
+		    	exit;
+			}
 
-		$enabled_order = "enabled";
-		$secret_key = $this->config->item('secret_key');
-		$decoded = JWT::decode($access_token, $secret_key, array('HS256'));
-		if ( !(isset($decoded->profile_data->patient_profile_id))) {
-			header("HTTP/1.1 401");
-	    	$data['code'] = "401";
-	    	$data['message'] = "INVALID TOKEN";
-	    	echo json_encode($data);
-	    	exit;
-		}
+			$enabled_order = "enabled";
+			$secret_key = $this->config->item('secret_key');
+			$decoded = JWT::decode($access_token, $secret_key, array('HS256'));
+			if ( !(isset($decoded->profile_data->patient_profile_id))) {
+				header("HTTP/1.1 401");
+		    	$data['code'] = "401";
+		    	$data['message'] = "INVALID TOKEN";
+		    	echo json_encode($data);
+		    	exit;
+			}
 
-		$patient_profile_id = $decoded->profile_data->patient_profile_id;
-		$qry_check_order = "SELECT * FROM order_patient WHERE 1 AND patient_id = ? order by id DESC LIMIT 0,1";
-		$run_check_order = $this->db->query($qry_check_order, array($patient_profile_id));
-		if ( $run_check_order->num_rows() > 0 ){
-			$data_order = $run_check_order->result_array();
-			if ( $data_order[0]['keluhan'] == 1 ){
-				$now = strtotime(date("Y-m-d H:i:s"));
-				$order_time = strtotime($data_order[0]['created_at']);
-				$diff = ($now - $order_time) / (60 * 60 * 24);
-				if ( $diff <= 7 ){
-					$enabled_order = "disabled";
+			$patient_profile_id = $decoded->profile_data->patient_profile_id;
+			$qry_check_order = "SELECT * FROM order_patient WHERE 1 AND patient_id = ? order by id DESC LIMIT 0,1";
+			$run_check_order = $this->db->query($qry_check_order, array($patient_profile_id));
+			if ( $run_check_order->num_rows() > 0 ){
+				$data_order = $run_check_order->result_array();
+				if ( $data_order[0]['keluhan'] == 1 ){
+					$now = strtotime(date("Y-m-d H:i:s"));
+					$order_time = strtotime($data_order[0]['created_at']);
+					$diff = ($now - $order_time) / (60 * 60 * 24);
+					if ( $diff <= 7 ){
+						$enabled_order = "disabled";
+					}
 				}
 			}
-		}
 
-		$data['code'] = "200";
-    	$data['message'] = "Success Profile";
-    	$data['token'] = $access_token;
-    	$data['order'] = $enabled_order;
+			$data['code'] = "200";
+	    	$data['message'] = "Success Profile";
+	    	$data['token'] = $access_token;
+	    	$data['order'] = $enabled_order;
 
+	    	echo json_encode($data);
+    	}
+
+    	$data['code'] = "200";
     	echo json_encode($data);
 	}
 
@@ -85,58 +93,65 @@ class Profile extends CI_Controller {
 		$obj = file_get_contents('php://input');
 		$edata = json_decode($obj);
 		$access_token = $_SERVER['HTTP_TOKEN'];
-		if ( $this->profile->check_token($access_token) == false ){
-			header("HTTP/1.1 401");
-			$data['code'] = "401";
-			$data['message'] = "INVALID TOKEN";
+
+		if ( $_SERVER['REQUEST_METHOD'] != "OPTIONS"){
+			if ( $this->profile->check_token($access_token) == false ){
+				header("HTTP/1.1 401");
+				$data['code'] = "401";
+				$data['message'] = "INVALID TOKEN";
+				echo json_encode($data);
+				exit();
+			}
+
+
+			if ( !(isset($edata->bpjs_number))) {
+				header("HTTP/1.1 401");
+				$data['code'] = "401";
+				$data['message'] = "INVALID REQUEST";
+				echo json_encode($data);
+				exit();
+			}
+
+			$check_bpjs = "SELECT pl.remember_token as remember_token, pp.id as patient_profile_id FROM patient_login as pl 
+				INNER JOIN patient_profile as pp ON (pl.id = pp.patient_login_id) 
+				WHERE 1 AND ( pl.no_bpjs = ? OR pl.no_medrec = ? ) ";
+			$run_bpjs = $this->db->query($check_bpjs,array($edata->bpjs_number, $edata->bpjs_number));
+			if ( $run_bpjs->num_rows() <= 0 ){
+				header("HTTP/1.1 401");
+				$data['code'] = "401";
+				$data['message'] = "Data can't retrieve";
+				echo json_encode($data);
+				exit();
+			}
+
+			$res_bpjs = $run_bpjs->result_array();
+			$profile_id = $res_bpjs[0]['patient_profile_id'];
+			$array_insert = array(
+				"patient_id" => $profile_id,
+				"delivery_date" => date("Y-m-d H:i:s"),
+				"created_at" => date("Y-m-d H:i:s"),
+				"status" => 1,
+				"keluhan" => $edata->complaint,
+				'description' => $edata->complaint_description
+			);
+			$this->db->insert("order_patient", $array_insert);
+
+			if ( $edata->complaint == "1" ){
+				header("HTTP/1.1 406");
+				$data['code'] = "406";
+				$data['message'] = "Order have complaint";
+				echo json_encode($data);
+				exit();
+			}
+
+			$data['code'] = "200";
+			$data['message'] = "Success Order";
 			echo json_encode($data);
-			exit();
-		}
-
-
-		if ( !(isset($edata->bpjs_number))) {
-			header("HTTP/1.1 401");
-			$data['code'] = "401";
-			$data['message'] = "INVALID REQUEST";
-			echo json_encode($data);
-			exit();
-		}
-
-		$check_bpjs = "SELECT pl.remember_token as remember_token, pp.id as patient_profile_id FROM patient_login as pl 
-			INNER JOIN patient_profile as pp ON (pl.id = pp.patient_login_id) 
-			WHERE 1 AND ( pl.no_bpjs = ? OR pl.no_medrec = ? ) ";
-		$run_bpjs = $this->db->query($check_bpjs,array($edata->bpjs_number, $edata->bpjs_number));
-		if ( $run_bpjs->num_rows() <= 0 ){
-			header("HTTP/1.1 401");
-			$data['code'] = "401";
-			$data['message'] = "Data can't retrieve";
-			echo json_encode($data);
-			exit();
-		}
-
-		$res_bpjs = $run_bpjs->result_array();
-		$profile_id = $res_bpjs[0]['patient_profile_id'];
-		$array_insert = array(
-			"patient_id" => $profile_id,
-			"delivery_date" => date("Y-m-d H:i:s"),
-			"created_at" => date("Y-m-d H:i:s"),
-			"status" => 1,
-			"keluhan" => $edata->complaint,
-			'description' => $edata->complaint_description
-		);
-		$this->db->insert("order_patient", $array_insert);
-
-		if ( $edata->complaint == "1" ){
-			header("HTTP/1.1 406");
-			$data['code'] = "406";
-			$data['message'] = "Order have complaint";
-			echo json_encode($data);
-			exit();
 		}
 
 		$data['code'] = "200";
-		$data['message'] = "Success Order";
 		echo json_encode($data);
+		
 	}
 
 	public function update_address(){
@@ -144,31 +159,36 @@ class Profile extends CI_Controller {
 		$edata = json_decode($obj);
 		$access_token = $_SERVER['HTTP_TOKEN'];
 
-		if ( $this->profile->check_token($access_token) == false ){
-			header("HTTP/1.1 401");
-			$data['code'] = "401";
-			$data['message'] = "INVALID TOKEN";
-			echo json_encode($data);
-			exit();
-		}
+		if ( $_SERVER['REQUEST_METHOD'] != "OPTIONS"){
+			if ( $this->profile->check_token($access_token) == false ){
+				header("HTTP/1.1 401");
+				$data['code'] = "401";
+				$data['message'] = "INVALID TOKEN";
+				echo json_encode($data);
+				exit();
+			}
 
 
-		if ( !(isset($edata->address))) {
-			header("HTTP/1.1 401");
-			$data['code'] = "401";
-			$data['message'] = "INVALID REQUEST";
+			if ( !(isset($edata->address))) {
+				header("HTTP/1.1 401");
+				$data['code'] = "401";
+				$data['message'] = "INVALID REQUEST";
+				echo json_encode($data);
+				exit();
+			}
+			$address = $edata->address;
+			$secret_key = $this->config->item('secret_key');
+			$status_order = $this->config->item('status_order');
+			$decoded = JWT::decode($access_token, $secret_key, array('HS256'));
+			$patient_profile_id = $decoded->profile_data->patient_profile_id;
+			$this->db->query("UPDATE patient_profile set address = '$address' where id = '$patient_profile_id'");
+
+			$data['code'] = "200";
+			$data['message'] = "Address has been updated";
 			echo json_encode($data);
-			exit();
 		}
-		$address = $edata->address;
-		$secret_key = $this->config->item('secret_key');
-		$status_order = $this->config->item('status_order');
-		$decoded = JWT::decode($access_token, $secret_key, array('HS256'));
-		$patient_profile_id = $decoded->profile_data->patient_profile_id;
-		$this->db->query("UPDATE patient_profile set address = '$address' where id = '$patient_profile_id'");
 
 		$data['code'] = "200";
-		$data['message'] = "Address has been updated";
 		echo json_encode($data);
 	}
 
